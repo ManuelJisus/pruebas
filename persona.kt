941b4762161cc9f6a677ffdf024d761329a5596f
ManuelJisus <91907544+ManuelJisus@users.noreply.github.com>
2021-11-03 21:40:12 +0100

    Update persona.kt

--- persona.kt
+++ persona.kt
@@ -1,20 +1,29 @@
 fun main() {
 
 }
-class Personas (   var nombre : String = "",   var edad: Int = 0,   var sexo: String = "H"){
-    var DNI : String
-    var peso : Int
-    var altura : Int
+
+class Personas(var nombre: String = "", var edad: Int = 0, var sexo: String = "H") {
+    var DNI: String
+    var peso: Double
+    var altura: Double
 
     init {
         DNI = ""
-        altura = 0
-        peso = 0
+        altura = 0.0
+        peso = 0.0
 
 
     }
-    constructor(DNI : String ): this(nombre, edad, sexo) {
 
+    constructor(DNI: String, altura: Double, peso: Double, nombre: String, edad: Int, sexo: String) : this(
+        nombre,
+        edad,
+        sexo
+    ) {
+
+        this.DNI = DNI
+        this.peso = peso
+        this.altura = altura
     }
 
 
