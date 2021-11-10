32c5c5da57f92dc0ec0630aeb85a557fb8cfc16a
ManuelJisus <91907544+ManuelJisus@users.noreply.github.com>
2021-11-10 21:40:18 +0100

    Update notas.kt

--- notas.kt
+++ notas.kt
@@ -16,8 +16,8 @@ class Modulo(var NumeroAlumnos: Int) {
     companion object{
         const val EV_PRIMERA = "0"
         const val ev_Segunda = "1"
-        const val ev_Tercera = "2"
-        const val ev_Final = "3"
+        const val EV_TERCERA = "2"
+        const val EV_FINAL = "3"
     }
     fun matricularAlumno(alumno: Alumno): Boolean {
 //**
@@ -33,23 +33,23 @@ class Modulo(var NumeroAlumnos: Int) {
         return false
     }
     fun establecerNota(idAlumno:String, evaluacion:String, nota:Float): Boolean{
-        if (evaluacion in Modulo.EV_PRIMERA..Modulo.ev_Final)
+        if (evaluacion in Modulo.EV_PRIMERA..Modulo.EV_FINAL)
         {
             var posicion = alumnos.indexOfFirst { it?.ID == idAlumno }
             if (posicion != -1){
                 // alumnos.set(posicion,alumno)
-                notasAlumnos [evaluacion.toInt()] [posicion]
+                notasAlumnos [evaluacion.toInt()] [posicion] = nota
 
                 return true
             }
-
+            return true
         }
-
-
+      
+            
             return false
         }
-
-
+        
+    
 }
 
  data class Alumno(var ID: String, var nombre: String, var apellido1: String, var apellido2: String) {
