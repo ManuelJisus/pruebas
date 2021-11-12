4585d30c5d522803965c0e5071473b10460cb319
ManuelJisus <91907544+ManuelJisus@users.noreply.github.com>
2021-11-12 19:34:42 +0100

    Update notas.kt

--- notas.kt
+++ notas.kt
@@ -15,14 +15,14 @@ class Modulo(var NumeroAlumnos: Int) {
     var evaluacion = 0
     companion object{
         const val EV_PRIMERA = "0"
-        const val ev_Segunda = "1"
+        const val EV_SEGUNDA = "1"
         const val EV_TERCERA = "2"
         const val EV_FINAL = "3"
     }
     fun matricularAlumno(alumno: Alumno): Boolean {
 //**
         if (alumnosMatriculados <= NumeroAlumnos) {
-           var posicion = alumnos.indexOfFirst { it ==null }
+            var posicion = alumnos.indexOfFirst { it ==null }
             if (posicion != -1){
                 // alumnos.set(posicion,alumno)
                 alumnos [posicion]= alumno
@@ -44,14 +44,29 @@ class Modulo(var NumeroAlumnos: Int) {
             }
             return true
         }
-      
-            
-            return false
-        }
-        
-    
+
+
+        return false
+    }
+
+    fun calcularEvaluacionFinal(idAlumno: String) {
+        var notasAlu = alumnos.indexOfFirst { it?.ID == idAlumno }
+        var media = 0.0f
+        var EV1 = notasAlumnos[notasAlu][0]
+        var EV2 = notasAlumnos[notasAlu][1]
+        var EV3 =notasAlumnos[notasAlu][2]
+
+        media = (EV1 + EV2 + EV3) / 3
+
+        media = notasAlumnos[notasAlu][3]
+    }
+   fun numeroAprobados(evaluacion:String): Int{
+       var aprobados=notasAlumnos [evaluacion.toInt()].count{it>=5}
+       return aprobados
+   }
+    fun notaMasBaja(evaluacion:String): Float
 }
 
- data class Alumno(var ID: String, var nombre: String, var apellido1: String, var apellido2: String) {
+data class Alumno(var ID: String, var nombre: String, var apellido1: String, var apellido2: String) {
 
 }
